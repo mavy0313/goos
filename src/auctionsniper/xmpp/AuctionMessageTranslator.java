f5ea628ea47635599a1fdb20376f6240431d2d0f
mavy0313 <vypoff@gmail.com>
2018-09-04 08:10:22 +0400

    Added unit test AuctionMessageTranslatorTest - passing

---
+++ src/auctionsniper/xmpp/AuctionMessageTranslator.java
@@ -0,0 +1,18 @@
+package auctionsniper.xmpp;
+
+import auctionsniper.AuctionEventListener;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+
+public class AuctionMessageTranslator implements MessageListener {
+    private AuctionEventListener listener;
+
+    public AuctionMessageTranslator(AuctionEventListener listener) {
+        this.listener = listener;
+    }
+
+    public void processMessage(Chat chat, Message message) {
+        this.listener.auctionClosed();
+    }
+}
